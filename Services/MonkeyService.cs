00856886f58c0530a67242ad171c79d3e55344f0
MiikaM <miika.mikkonen5@gmail.com>
2023-01-11 20:31:01 +0200

    Added CommunitytoolkitMvvm for build targets

--- Services/MonkeyService.cs
+++ Services/MonkeyService.cs
@@ -3,4 +3,5 @@ namespace MAUIMonkeyFinder.Services;
 
 public class MonkeyService
 {
+
 }
--- ViewModel/BaseViewModel.cs
+++ ViewModel/BaseViewModel.cs
@@ -1,5 +1,60 @@
-﻿namespace MAUIMonkeyFinder.ViewModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class BaseViewModel
+namespace MAUIMonkeyFinder.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !isBusy;
+
+    //public bool IsBusy
+    //{
+    //    get => isBusy;
+    //    set
+    //    {
+    //        if (isBusy == value)
+    //        {
+    //            return;
+    //        }
+    //        isBusy = value;
+    //        OnPropertyChanged();
+    //        OnPropertyChanged(nameof(IsNotBusy));
+    //    }
+    //}
+
+    //public string Title
+    //{
+    //    get => title;
+    //    set
+    //    {
+    //        if (title == value)
+    //        {
+    //            return;
+    //        }
+    //        title = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+
+    //public bool IsNotBusy => !isBusy;
+
+
+    //public event PropertyChangedEventHandler PropertyChanged;
+
+    //public void OnPropertyChanged([CallerMemberName] string name = null)
+    //{
+    //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    //}
+
 }
